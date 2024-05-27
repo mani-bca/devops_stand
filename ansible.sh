760e4e623e53af3bfcc26efbdf70a633745da9a8
Dood <manidevops2021@gmail.com>
2024-05-27 10:59:55 +0530

    change5

--- ansible.sh
+++ ansible.sh
@@ -4,3 +4,8 @@ sudo apt update
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
+
+35.154.184.111    172.31.1.161    - grafana_promothues
+
+13.200.229.192    172.31.8.95   - node
